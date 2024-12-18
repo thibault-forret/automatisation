405e5c15a042d25df38f690170034beadafd502c
thibault_f <78164277+thibault-forret@users.noreply.github.com>
2024-12-19 00:16:11 +0100

    factorize insert into db, need to check for minio

--- api-csharp/CalculController.cs
+++ api-csharp/CalculController.cs
@@ -9,9 +9,11 @@ using Minio.DataModel.Args;
 using Minio.Exceptions;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 // Commenter tous le code
 // Factoriser le code
+// Faire de meilleurs messages de retour
 
 namespace MonProjetAPI.Controllers
 {
@@ -29,94 +31,109 @@ namespace MonProjetAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> PostResult([FromBody] CalculDto dto)
         {
-            // Configuration MinIO
-            var endpoint = "minio:9000"; // Adresse MinIO
-            var accessKey = "admin";         // Identifiant
-            var secretKey = "admin123";      // Mot de passe
-            var bucketName = "syracuse";     // Nom du bucket
 
-            // Configuration du client MinIO
-            var minioClient = new MinioClient()
-                .WithEndpoint(endpoint)
-                .WithCredentials(accessKey, secretKey)
-                .Build();
+            try {
+                // await SaveIntoBucket(dto);
 
-            // Vérifier si le bucket existe, sinon le créer
-            bool found = await minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(bucketName));
-            if (!found)
-            {
-                Console.WriteLine($"Bucket '{bucketName}' not found. Creating...");
-                await minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket(bucketName));
+                SaveIntoDatabase(dto);
+            } catch (Exception ex) {
+                return BadRequest(new { error = ex });
             }
 
-            var fileName = $"{dto.Nombre}.txt"; // Nom du fichier basé sur le nombre
-            var fileContent = string.Join(", ", dto.Syracuse); // Contenu du fichier
-            var fileBytes = Encoding.UTF8.GetBytes(fileContent);
+            return Ok(new { message = "Résultat stockés avec succès", result = dto });
+            
 
-            // Stocker le fichier
-            using (var memoryStream = new MemoryStream(fileBytes))
-            {
-                Console.WriteLine($"Uploading {fileName} to bucket {bucketName}...");
-                await minioClient.PutObjectAsync(new PutObjectArgs()
-                    .WithBucket(bucketName)
-                    .WithObject(fileName)
-                    .WithStreamData(memoryStream)
-                    .WithObjectSize(memoryStream.Length)
-                    .WithContentType("text/plain"));
-            }
+            // // Configuration MinIO
+            // var endpoint = "minio:9000"; // Adresse MinIO
+            // var accessKey = "admin";         // Identifiant
+            // var secretKey = "admin123";      // Mot de passe
+            // var bucketName = "syracuse";     // Nom du bucket
+
+            // // Configuration du client MinIO
+            // var minioClient = new MinioClient()
+            //     .WithEndpoint(endpoint)
+            //     .WithCredentials(accessKey, secretKey)
+            //     .Build();
+
+            // // Vérifier si le bucket existe, sinon le créer
+            // bool found = await minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(bucketName));
+            // if (!found)
+            // {
+            //     Console.WriteLine($"Bucket '{bucketName}' not found. Creating...");
+            //     await minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket(bucketName));
+            // }
+
+            // var fileName = $"{dto.Nombre}.txt"; // Nom du fichier basé sur le nombre
+            // var fileContent = string.Join(", ", dto.Syracuse); // Contenu du fichier
+            // var fileBytes = Encoding.UTF8.GetBytes(fileContent);
+
+            // // Si déjà stocker, ne pas upload le fichier (même si ca remplace le fichier d'origine sur le bucket)
+            // // -> Le récupérer
+
+            // // Stocker le fichier
+            // using (var memoryStream = new MemoryStream(fileBytes))
+            // {
+            //     Console.WriteLine($"Uploading {fileName} to bucket {bucketName}...");
+            //     await minioClient.PutObjectAsync(new PutObjectArgs()
+            //         .WithBucket(bucketName)
+            //         .WithObject(fileName)
+            //         .WithStreamData(memoryStream)
+            //         .WithObjectSize(memoryStream.Length)
+            //         .WithContentType("text/plain"));
+            // }
 
             // -------------------- BDD --------------------
 
             //Se connecter au serveur
-            string connectionString = "Server=db;Port=3306;Database=calculs;User=calcul_user;Password=1234;";
+            // string connectionString = "Server=db;Port=3306;Database=calculs;User=calcul_user;Password=1234;";
 
-            try
-            {
-                using (var connection = new MySqlConnection(connectionString))
-                {
-                    connection.Open();
+            // try
+            // {
+            //     using (var connection = new MySqlConnection(connectionString))
+            //     {
+            //         connection.Open();
 
-                    // Vérifier si le nombre existe déjà
-                    string checkQuery = "SELECT COUNT(*) FROM calcul_results WHERE nombre = @nombre";
-                    using (var checkCommand = new MySqlCommand(checkQuery, connection))
-                    {
-                        checkCommand.Parameters.Add(new MySqlParameter("@nombre", dto.Nombre));
-                        int count = Convert.ToInt32(checkCommand.ExecuteScalar()); 
+            //         // Vérifier si le nombre existe déjà
+            //         string checkQuery = "SELECT COUNT(*) FROM calcul_results WHERE nombre = @nombre";
+            //         using (var checkCommand = new MySqlCommand(checkQuery, connection))
+            //         {
+            //             checkCommand.Parameters.Add(new MySqlParameter("@nombre", dto.Nombre));
+            //             int count = Convert.ToInt32(checkCommand.ExecuteScalar()); 
 
-                        if (count > 0)
-                        {
-                            return Ok(new { message = "Résultat reçu mais non stocké (existe déjà)", result = dto });
-                        }
-                    }
+            //             if (count > 0)
+            //             {
+            //                 return Ok(new { message = "Résultat reçu mais non stocké (existe déjà)", result = dto });
+            //             }
+            //         }
 
-                    // Insérer le nouveau résultat
-                    string insertQuery = "INSERT INTO calcul_results (nombre, pair, premier, parfait, created_at) " +
-                                        "VALUES (@nombre, @pair, @premier, @parfait, @created_at)";
-                    using (var insertCommand = new MySqlCommand(insertQuery, connection))
-                    {
-                        insertCommand.Parameters.Add(new MySqlParameter("@nombre", dto.Nombre));
-                        insertCommand.Parameters.Add(new MySqlParameter("@pair", dto.Pair));
-                        insertCommand.Parameters.Add(new MySqlParameter("@premier", dto.Premier));
-                        insertCommand.Parameters.Add(new MySqlParameter("@parfait", dto.Parfait));
-                        insertCommand.Parameters.Add(new MySqlParameter("@created_at", DateTime.UtcNow));
+            //         // Insérer le nouveau résultat
+            //         string insertQuery = "INSERT INTO calcul_results (nombre, pair, premier, parfait, created_at) " +
+            //                             "VALUES (@nombre, @pair, @premier, @parfait, @created_at)";
+            //         using (var insertCommand = new MySqlCommand(insertQuery, connection))
+            //         {
+            //             insertCommand.Parameters.Add(new MySqlParameter("@nombre", dto.Nombre));
+            //             insertCommand.Parameters.Add(new MySqlParameter("@pair", dto.Pair));
+            //             insertCommand.Parameters.Add(new MySqlParameter("@premier", dto.Premier));
+            //             insertCommand.Parameters.Add(new MySqlParameter("@parfait", dto.Parfait));
+            //             insertCommand.Parameters.Add(new MySqlParameter("@created_at", DateTime.UtcNow));
 
-                        int rowsAffected = insertCommand.ExecuteNonQuery();
+            //             int rowsAffected = insertCommand.ExecuteNonQuery();
 
-                        if (rowsAffected > 0)
-                        {
-                            return Ok(new { message = "Résultat reçu et stocké avec succès", result = dto });
-                        }
-                        else
-                        {
-                            return BadRequest(new { error = "Échec de l'insertion dans la base de données" });
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { error = "Erreur lors de la sauvegarde en base de données", details = ex.Message });
-            }
+            //             if (rowsAffected > 0)
+            //             {
+            //                 return Ok(new { message = "Résultat reçu et stocké avec succès", result = dto });
+            //             }
+            //             else
+            //             {
+            //                 return BadRequest(new { error = "Échec de l'insertion dans la base de données" });
+            //             }
+            //         }
+            //     }
+            // }
+            // catch (Exception ex)
+            // {
+            //     return BadRequest(new { error = "Erreur lors de la sauvegarde en base de données", details = ex.Message });
+            // }
         }
 
         public class CalculDto
@@ -126,6 +143,129 @@ namespace MonProjetAPI.Controllers
             public bool Premier { get; set; }
             public bool Parfait { get; set; }
             public List<int> Syracuse { get; set; }
+        }
+
+
+        // Faire le summary
+        static private async Task SaveIntoBucket(CalculDto dto) 
+        {
+            var endpoint = "minio:9000"; // Adresse MinIO
+            var accessKey = "admin";         // Identifiant
+            var secretKey = "admin123";      // Mot de passe
+            var bucketName = "syracuse";     // Nom du bucket
+
+            try {
+                // Configuration du client MinIO
+                var minioClient = new MinioClient()
+                    .WithEndpoint(endpoint)
+                    .WithCredentials(accessKey, secretKey)
+                    .Build();
+
+                // Vérifier si le bucket existe, sinon le créer
+                await IfBucketDoesntExistsCreateIt(minioClient, bucketName);
+
+                // Stocker le fichier dans le bucket
+                await SaveFileIntoBucket(minioClient, bucketName, dto);
+            } catch (Exception ex) {
+                throw new Exception("Erreur lors du téléchargement dans MinIO", ex);
+            }
+
+            
+        }
+
+        static private async Task IfBucketDoesntExistsCreateIt(IMinioClient minioClient, string bucketName)
+        {
+            bool found = await minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(bucketName));
+            if (!found)
+            {
+                await minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket(bucketName));
+            }
+        }
+
+        // Si déjà stocker, ne pas upload le fichier (même si ca remplace le fichier d'origine sur le bucket)
+        // -> Le récupérer
+        static private async Task SaveFileIntoBucket(IMinioClient minioClient, string bucketName, CalculDto dto)
+        {
+            var fileName = $"{dto.Nombre}.txt"; // Nom du fichier basé sur le nombre
+            var fileContent = string.Join(", ", dto.Syracuse); // Contenu du fichier
+            var fileBytes = Encoding.UTF8.GetBytes(fileContent);
+
+            // Stocker le fichier
+            using var memoryStream = new MemoryStream(fileBytes);
+
+            await minioClient.PutObjectAsync(new PutObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(fileName)
+                .WithStreamData(memoryStream)
+                .WithObjectSize(memoryStream.Length)
+                .WithContentType("text/plain"));
+        }
+
+        // Faire le summary
+        static private void SaveIntoDatabase(CalculDto dto)
+        {
+            string connectionString = "Server=db;Port=3306;Database=calculs;User=calcul_user;Password=1234;";
+
+            try
+            {
+                using var connection = new MySqlConnection(connectionString);
+
+                connection.Open();
+
+                // Vérifier si les informations sont déjà stockées dans la BDD
+                var result = VerifyIfDataAlreadySave(connection, dto);
+
+                // Insérer si ce n'est pas stocker, sinon ne rien faire
+                if (!result) {
+                    // Insérer le nouveau résultat
+                    var insert = InsertDataIntoDatabase(connection, dto);
+
+                    // Vérifie si l'inserstion s'est bien dérouler
+                    if (!insert)
+                        throw new Exception("Un problème est survenu lors de l'insertion");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur lors de la sauvegarde en base de données", ex);
+            }
+        }
+
+        // Vérifier si le nombre est déjà stocker dans la BDD
+        static private bool VerifyIfDataAlreadySave(MySqlConnection connection, CalculDto dto) {
+            string checkQuery = "SELECT COUNT(*) FROM calcul_results WHERE nombre = @nombre";
+
+            using var checkCommand = new MySqlCommand(checkQuery, connection);
+
+            checkCommand.Parameters.Add(new MySqlParameter("@nombre", dto.Nombre));
+
+            int count = Convert.ToInt32(checkCommand.ExecuteScalar());
+
+            if (count > 0) // Vérifie si une ligne est retournée
+                return true;
+
+            return false; // Si aucune donnée n'est trouvée
+        }
+
+        static private bool InsertDataIntoDatabase(MySqlConnection connection, CalculDto dto)
+        {
+            string insertQuery = "INSERT INTO calcul_results (nombre, pair, premier, parfait, created_at) " +
+                                    "VALUES (@nombre, @pair, @premier, @parfait, @created_at)";
+
+            using var insertCommand = new MySqlCommand(insertQuery, connection);
+
+            insertCommand.Parameters.Add(new MySqlParameter("@nombre", dto.Nombre));
+            insertCommand.Parameters.Add(new MySqlParameter("@pair", dto.Pair));
+            insertCommand.Parameters.Add(new MySqlParameter("@premier", dto.Premier));
+            insertCommand.Parameters.Add(new MySqlParameter("@parfait", dto.Parfait));
+            insertCommand.Parameters.Add(new MySqlParameter("@created_at", DateTime.UtcNow));
+
+            int rowsAffected = insertCommand.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+                return true;
+            else
+                return false;
         }
     }
 }
